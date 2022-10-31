f6df05bca096573cde1ca474ab026e991b0b5dae
Breno Xavier <brenoxavier1227@gmail.com>
2022-10-31 09:01:12 -0300

    Removing non-existing method

--- src/graph.ts
+++ src/graph.ts
@@ -2,8 +2,7 @@ const addon = require('../build/Release/Addon')
 
 export interface Graph {
     setNode(x: number, z: number, block: number): void,
-    getNode(x: number, z: number): number,
-    print(): void
+    getNode(x: number, z: number): number
 }
 
 export const Graph: {
